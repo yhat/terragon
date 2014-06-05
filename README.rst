009ea9f21cc492a7f42acf5ebd14053adef3b0f2
Greg Lamp <lamp.greg@gmail.com>
2014-06-05 13:24:39 -0400

    python packing really sucks

--- README.rst
+++ README.rst
@@ -4,3 +4,21 @@ terragon
 "Sub-fork" of the
 ```cloud`` <https://pypi.python.org/pypi/cloud/2.3.1>`__ package's
 cloudpickle.
+
+Install
+-------
+
+.. code:: bash
+
+    $ pip install terragon
+
+Use
+---
+
+Same as pickle
+
+.. code:: python
+
+    >>> import terragon
+    >>> terragon.dumps({ "x": range(10) })
+
--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ required = []
 
 setup(
     name="terragon",
-    version="0.1",
+    version="0.1.3",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/terragon/",
     license="BSD",
     packages=find_packages(),
-    package_dir={"terragon": "terragon"},
+    package_data={'': ['README.rst']},
     description="a better pickle",
     long_description=open("README.rst").read(),
     install_requires=required,
